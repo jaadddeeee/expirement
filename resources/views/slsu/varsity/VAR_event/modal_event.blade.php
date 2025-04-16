38371f75cc4df58fbf990078a69190c765fd7735
thonexdp <pazjeck14@gmail.com>
2025-04-16 10:52:38 +0800

    fix the indentation

--- resources/views/slsu/varsity/VAR_event/modal_event.blade.php
+++ resources/views/slsu/varsity/VAR_event/modal_event.blade.php
@@ -9,10 +9,10 @@
             </div>
             <div class="modal-body">
                 <form id="frmAdd">
-                  @csrf
-                  <div id="msg"></div>
+                @csrf
+                <div id="msg"></div>
                     <div class = "form-group">
-                      <input type="text" name = "event" id="event" class = "mb-4 form-control" placeholder = "Event">
+                    <input type="text" name = "event" id="event" class = "mb-4 form-control" placeholder = "Event">
                     </div>
                 </form>
             </div>
@@ -26,26 +26,26 @@
 
 {{-- update modal --}}
 <div class="modal fade" id="updateModalEvent" tabindex="-1" aria-labelledby="eventModal" aria-hidden="true" style="display: none;">
-  <div class="modal-dialog modal-sm">
-      <div class="modal-content">
-          <div class="modal-header">
-              <h5 class="modal-title h4 text-warning" id="eventModalLabel">Update Event</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-              <form id="frmUpdate">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title h4 text-warning" id="eventModalLabel">Update Event</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+        <div class="modal-body">
+            <form id="frmUpdate">
                 @csrf
                 <div id="updatemsg"></div>
                 <input hidden type = "text" name = "hiddentID" id="hiddentID" value="">
-                  <div class = "form-group">
-                    <input type="text" name = "updateEvent" id="updateEvent" class = "mb-4 form-control" placeholder = "Event">
-                  </div>
-              </form>
-          </div>
-          <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-warning" id="btn-update">Update</button>
-          </div>
-      </div>
-  </div>
+                    <div class = "form-group">
+                        <input type="text" name = "updateEvent" id="updateEvent" class = "mb-4 form-control" placeholder = "Event">
+                    </div>
+            </form>
+        </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-warning" id="btn-update">Update</button>
+            </div>
+        </div>
+    </div>
 </div>
